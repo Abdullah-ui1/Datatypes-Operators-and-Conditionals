369caa8c96a06cd8ee9f4726118a9bf043b39e21
Abdullah-ui1 <abdullahali89@outlook.com>
2022-10-05 16:15:38 +0100

    Create Datatypes, Operators, Conditionals.js

---
+++ Datatypes, Operators, Conditionals.js
@@ -0,0 +1,87 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+// Strings - These are statements in your code which can be made up of letters, numbers and symbols, encompassed by "---" or '---'
+// Numbers - These are used to purely hold a number value
+// Booleans - These can only hold 2 values, True or False, almost like an on/off switch
+// Objects - These can be attributed properties to define their characteristics
+// Null - This refers to a variable that has no value
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+*/
+
+// A variable is a method used to store data values that can be changed and manipulated later on.
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+// ------ let sum1 = 12 + 20;
+// ------ console.log(sum1);
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+let x = 12;
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+// ------ let name = "John Doe";
+// ------ console.log(name);
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+// ------ let newSum = x - 12;
+// ------ console.log(newSum);
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+*/
+
+// ------ let name1 = "john";
+// ------ let name2 = "John";
+
+// ------ let name3 = name1 === name2 && true;
+// ------ console.log("The names are not the same, it is", name3);
+
+// ------ let name4 = name1 === "john" || name2 === "John";
+
+// ------ console.log("The names match, it is", name4);
+
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+// function getPrice(Item) {
+//   return Item ? "15" : "30";
+// }
+// console.log(getPrice(true));
+// console.log(getPrice(false));
+// console.log(getPrice(null));
+
+/* WRITE YOUR ANSWER HERE */
